ffe99b98a6b093d5870032a91d0838ced1d7e709
Daniel Vianna <dmlvianna@gmail.com>
2016-10-21 06:21:18 +0000

    Vectors

---
+++ src/fusion.hs
@@ -0,0 +1,24 @@
+
+module Fusion where
+
+import Criterion.Main
+import qualified Data.Vector as V
+
+testV' :: Int -> V.Vector Int
+testV' n =
+    V.map (+n) $ V.map (+n) $
+     V.map (+n) $ V.map (+n)
+      (V.fromList [1..10000])
+
+testV :: Int -> V.Vector Int
+testV n =
+    V.map ( (+n) . (+n)
+          . (+n) . (+n) ) (V.fromList [1..10000])
+
+main :: IO ()
+main = defaultMain
+       [ bench "vector map prefused" $
+         whnf testV 9998
+       , bench "vector map will be fused" $
+         whnf testV' 9998
+       ] 
