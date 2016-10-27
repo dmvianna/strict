313c7695aaa1f3036d8ed741d7a293c813445a42
Daniel Vianna <dmlvianna@gmail.com>
2016-10-27 01:44:21 +0000

    boxed vs unboxed

--- src/boxed.hs
+++ src/boxed.hs
@@ -5,8 +5,8 @@ import Criterion.Main
 import qualified Data.List as L
 import qualified Data.Vector as B
 import qualified Data.Vector.Unboxed as U
-import qualified Data.Vector.Generic as G
-import qualified Data.Vector.Generic.Mutable as GM
+-- import qualified Data.Vector.Generic as G
+-- import qualified Data.Vector.Generic.Mutable as GM
 import Data.Word (Word64)
 import System.Random.Mersenne.Pure64
 
@@ -18,5 +18,16 @@ randomStream rndStep = L.unfoldr (Just . rndStep)
 toStream :: Word64 -> [Double]
 toStream seed = randomStream randomDouble $ pureMT seed
 
+boxed :: B.Vector Double
+boxed = B.fromList . take 10000 $ toStream 42
+
+unboxed :: U.Vector Double
+unboxed = U.fromList . take 10000 $ toStream 42
+
 main :: IO ()
-main = print . take 10 $ toStream 42
+main = defaultMain
+       [ bench "slicing boxed vector" $
+         whnf (B.head . B.slice 100 900) boxed
+       , bench "slicing unboxed vector" $
+         whnf (U.head . U.slice 100 900) unboxed
+       ]
