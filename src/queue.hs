ae0de31a989c565b5fb54099f5ea02b509022da6
Daniel Vianna <dmlvianna@gmail.com>
2016-11-04 04:57:13 +0000

    Queue vs Sequence -- queue doesn't turn.

--- src/queue.hs
+++ src/queue.hs
@@ -1,6 +1,10 @@
 
 module Queue where
 
+import Criterion.Main
+import Data.Maybe (fromJust)
+import qualified Data.Sequence as S
+
 -- From Okasaki's Purely Functional Data Structures
 data Queue a =
   Queue { enqueue :: [a]
@@ -10,7 +14,6 @@ data Queue a =
 toQueue :: [a] -> Queue a
 toQueue xs = Queue xs (reverse xs)
 
--- adds an item
 push :: a -> Queue a -> Queue a
 push x q = let xs = x : enqueue q
            in toQueue xs
@@ -21,3 +24,42 @@ pop q =
     [] -> Nothing
     x:xs -> Just (x, toQueue xs)
 
+-- Do stuff
+
+turnQ :: Queue a -> Queue a
+turnQ q' =
+  case dequeue q' of
+    [] -> toQueue []
+    _  -> let (x, q) = fromJust $ pop q -- living la vida loca
+          in push x q
+
+turnS :: S.Seq a -> S.Seq a
+turnS s =
+  if null s
+  then S.empty
+  else
+    let x  = S.index s 0
+        xs = snd $ S.splitAt 1 s
+    in xs S.|> x
+
+-- Sample
+
+l' :: [Int]
+l' = [1,2,3]
+
+q'' :: Queue Int
+q'' = toQueue l'
+
+s' :: S.Seq Int
+s' = S.fromList l'
+
+again :: Int -> (a -> a) -> a -> a
+again n f = foldr (.) id (replicate n f)
+
+main :: IO ()
+main = defaultMain
+  [ bench "turning queue" $
+    whnf (again 1 turnQ) q''
+  , bench "turning sequence" $
+    whnf (again 1 turnS) s'
+  ]
